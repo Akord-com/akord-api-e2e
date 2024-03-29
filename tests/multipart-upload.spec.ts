802ed06867c63714f5aa7f7cef00389be77410e2
Michal Kaliszewski <michal.kaliszewski00@gmail.com>
2024-03-29 15:49:06 +0100

    feat: add multipart upload test

--- tests/multipart-upload.spec.ts
+++ tests/multipart-upload.spec.ts
@@ -1,82 +1,71 @@
 require('dotenv').config();
 import { spec, request } from 'pactum';
-import { gt, regex } from 'pactum-matchers';
-
-const FILE_ID_REGEXP = /[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i;
-const TX_ID_REGEXP = /[a-zA-Z0-9_-]+$/i;
-
-const GATEWAY_URL_REGEXP = /^https:\/\/akrd\.net\/[a-zA-Z0-9_-]+$/i;
-const ARIO_GATEAY_URL_REGEXP = /^https:\/\/arweave\.net\/[a-zA-Z0-9_-]+$/i;
-const VIEWBLOCK_URL_REGEXP = /^https:\/\/viewblock\.io\/arweave\/tx\/[a-zA-Z0-9_-]+$/i;
-const FILE_STATUS_URL_REGEXP = /^https:\/\/api\.akord\.com\/files\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/status$/i;
-const FILE_URL_REGEXP = /^https:\/\/api\.akord\.com\/files\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i;
-
-const DOCS_URL = 'https://docs.akord.com/api-and-dev-tools/build/akord-api/files';
-const INFO_TEXT = "Transaction is visible on the blockchain indexers when in the \"committed\" status.";
-
+import fs from 'fs';
+import { arrayBuffer } from 'node:stream/consumers';
 
 const firstTagName = 'lowercase-tag-name';
 const firstTagValue = 'some tag';
-const secondTagName = 'Uppercase-Tag-Name';
-const secondTagValue = 'Other Tag';
-
-const contentTypeStandardTagName = 'Content-Type';
-const contentTypeStandardTagValue = 'text/plain';
 
 const tags = [
     {
         "name": firstTagName,
         "value": firstTagValue
-    },
-    {
-        "name": secondTagName,
-        "value": secondTagValue
-    },
-    {
-        "name": contentTypeStandardTagName,
-        "value": contentTypeStandardTagValue
     }
 ]
-
 const tagsBase64Encoded = Buffer.from(JSON.stringify(tags)).toString('base64');
 
-const APS_response_with_tags = {
-    "id": regex(FILE_ID_REGEXP),
-    "mimeType": 'text/plain',
-    "sizeInBytes": gt(0),
-    "cloud": {
-        uri: regex(FILE_ID_REGEXP),
-        url: regex(FILE_URL_REGEXP)
-    },
-    "tx": {
-        "id": regex(TX_ID_REGEXP),
-        "status": "scheduled",
-        "tags": tags,
-        "statusUrl": regex(FILE_STATUS_URL_REGEXP),
-        "gatewayUrl": regex(GATEWAY_URL_REGEXP),
-        "arioGatewayUrl": regex(ARIO_GATEAY_URL_REGEXP),
-        "viewblockUrl": regex(VIEWBLOCK_URL_REGEXP),
-        "infoUrl": DOCS_URL,
-        "info": INFO_TEXT
-    }
-}
-
-const ACS_response = {
-    "id": regex(FILE_ID_REGEXP),
-    "mimeType": 'text/plain',
-    "sizeInBytes": gt(0),
-    "cloud": {
-        uri: regex(FILE_ID_REGEXP),
-        url: regex(FILE_URL_REGEXP)
-    }
-}
+const CHUNK_SIZE = 1024 * 1024 * 5; // 5MB
+const FILE_PATH = './tests/data/20mb.pdf';
 
 describe('Files API', () => {
     before(async () => {
-        request.setDefaultTimeout(20000);
+        request.setDefaultTimeout(30000);
         request.setBaseUrl(process.env.BASE_URL as string);
     });
 
-    it.skip('should upload file to ACS (Akord Cloud Storage) usnig multipart upload', async () => {
+    it('should upload file to ACS (Akord Cloud Storage) usnig multipart upload', async () => {
+        // Get total file size
+        const stats = await fs.promises.stat(FILE_PATH);
+        const fileSize = stats.size;
+
+        const uploadChunk = async (start: number, end: number, headers: HeadersInit = {}) => {
+            const chunkStream = fs.createReadStream('./tests/data/20mb.pdf', { start: start, end: end });
+            const chunk = await arrayBuffer(chunkStream)
+            console.log(chunk.byteLength)
+            const response = await fetch(`${process.env.BASE_URL}/files?tags=${tagsBase64Encoded}`, {
+                method: 'POST',
+                headers: {
+                    'Api-Key': process.env.API_KEY as string,
+                    'Content-Type': 'application/pdf',
+                    'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                    ...headers
+                },
+                body: chunk
+            });
+            console.log("Uploaded chunk: " + `bytes ${start}-${end}/${fileSize}`)
+
+            if (response.status !== 202) {
+                throw new Error('Failed to upload first chunk of the file. Status code: ' + response.status);
+            }
+            return response;
+        }
+        // Upload first chunk of the file
+        const response = await uploadChunk(0, CHUNK_SIZE);
+
+        // Read location of the multipart upload
+        const contentLocation = response.headers.get('Content-Location');
+        if (!contentLocation) {
+            throw new Error('Content-Location header is missing');
+        }
+
+        // Upload middle chunks of the file using 'Content-Location' & 'Content-Range' - can be done concurrently
+        let sourceOffset = CHUNK_SIZE;
+        while (sourceOffset + CHUNK_SIZE < fileSize) {
+            await uploadChunk(sourceOffset, sourceOffset + CHUNK_SIZE, { 'Content-Location': contentLocation });
+            sourceOffset += CHUNK_SIZE;
+        }
+
+        // Upload last chunk of the file to complete the multipart upload
+        await uploadChunk(sourceOffset, fileSize, { 'Content-Location': contentLocation });
     });
 });
